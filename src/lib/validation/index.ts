318f54a7393e63a3af80b76804e1e61f4c7939c0
adityakch <adityakch09@gmail.com>
2024-09-03 11:39:48 -0400

    Home page

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -1,13 +1,17 @@
 import { z } from "zod";
 
 export const SignupValidation = z.object({
-    name: z.string().min(2, {message: "Too short"}),
-    username: z.string().min(2,{message: "Too short"}),
+    name: z.string().min(2, { message: "Too short" }),
+    username: z.string().min(2, { message: "Too short" }),
     email: z.string().email(),
-    password: z.string().min(8, {message: 'password must be atleast 8 characters'}),
-  });
+    password: z
+        .string()
+        .min(8, { message: "password must be atleast 8 characters" }),
+});
 
-  export const SigninValidation = z.object({
+export const SigninValidation = z.object({
     email: z.string().email(),
-    password: z.string().min(8, {message: 'password must be atleast 8 characters'}),
-  });
+    password: z
+        .string()
+        .min(8, { message: "password must be atleast 8 characters" }),
+});
